3ae633e85cda6766d1391cdf1fb040bbb49eb208
ikaynar <96616325+ikaynar@users.noreply.github.com>
2022-02-25 16:49:03 +0300

    Update load_data.py

--- workspace/ImageClassifier/utils/load_data.py
+++ workspace/ImageClassifier/utils/load_data.py
@@ -1,9 +1,8 @@
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
-
+import torch
 def load_data(data_dir):
-    
     data_transforms= transforms.Compose([
         transforms.Resize((224,224)),
         transforms.ToTensor(),
@@ -18,8 +17,10 @@ def load_data(data_dir):
     val_dataset = ImageFolder(valid_dir, transform=data_transforms)
     test_dataset = ImageFolder(test_dir, transform=data_transforms)
     image_dataset = (train_dataset, val_dataset, test_dataset)
-    
-    
+    train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+    valid_data_loader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=True)
+    test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=True)
+
     #img = transfer_test_transforms(img)
     
-    return 
+    return train_data_loader,valid_data_loader,test_data_loader
